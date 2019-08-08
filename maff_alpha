0fe7fcd6f1a600c3c102be087d2395f714f08bca
kawtar el-alami <kawtarelalami737@gmail.com>
2019-08-08 12:10:45 -0400

    1 try

---
+++ maff_alpha
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    Write(1 , "aBcDeFgHiJkLmNoPqRsTuVwXyz", 26);
+
+    return 0;
+}
---
+++ only_z.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+
+ int main ()
+ {
+       int z = 1 ;
+       char c ;
+       write(z, &c ,1);
+
+   return (0);
+
+ }
